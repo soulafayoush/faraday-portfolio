e8f0d526b39fd2d930791534b4acdf193dbf5f9e
TheBackenders <user.em200~git@github.com:TheBackenders/Group2_repo.git~>
2026-06-04 00:50:53 +0300

    UPDATE tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import tailwindcssAnimate from "tailwindcss-animate";
+
 
 const config: Config = {
     darkMode: "class",
@@ -57,6 +57,6 @@ const config: Config = {
                 }
         }
   },
-  plugins: [tailwindcssAnimate],
+ 
 };
 export default config;
